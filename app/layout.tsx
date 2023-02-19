facba223327648251c7ee36004a2924621f7143c
Damien Richcreek <damien@richcreek.dev>
2023-02-19 01:21:06 -0800

    Updated login, logout, firebase integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import './globals.css'
+import { AuthContextProvider } from '@/context/AuthContext'
 
 export default function RootLayout({
   children,
@@ -12,7 +15,11 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
+      </body>
     </html>
   )
 }
